b881223bc5518a45fc19322615b3b1196d1cb7dc
STNeto1 <gfreitasneto18@gmail.com>
2023-09-29 21:53:52 -0300

    form and form submission

--- handlers.go
+++ handlers.go
@@ -6,8 +6,22 @@ func HandleIndex(c *fiber.Ctx) error {
 	return c.Render("pages/index", fiber.Map{}, "layouts/main")
 }
 
+type CreateShortURLRequest struct {
+	URL string `form:"url,required"`
+}
+
 func HandleCreateShortURL(c *fiber.Ctx) error {
-	return c.SendString("Create Short URL")
+	var body CreateShortURLRequest
+
+	if err := c.BodyParser(&body); err != nil {
+		return c.Render("pages/index", fiber.Map{
+			"error": err.Error(),
+		})
+	}
+
+	return c.Render("pages/index", fiber.Map{
+		"message": "URL shortened!",
+	}, "layouts/main")
 }
 
 func HandleRedirect(c *fiber.Ctx) error {
